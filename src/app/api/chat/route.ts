72fbce2491291ee32e621090a599f51f6e2dbb3c
vyktwarh <72117738+vyktwarh@users.noreply.github.com>
2025-11-20 18:29:35 -0800

    Update route.ts

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -10,7 +10,7 @@ export async function POST(request: Request) {
     const { messages } = await request.json();
 
     const completion = await openai.chat.completions.create({
-      model: 'ft:gpt-3.5-turbo-0613:personal:ai-poet:88GTGA7b',
+      model: 'ft:gpt-4.1-nano-2025-04-14:personal:choreo-ai:CdUiTWvR',
       messages,
       stream: false,
     });
